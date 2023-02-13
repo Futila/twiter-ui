ef0805a61d237c74b8b9d58ec1ea178884b7b721
Fernando Futila <fernando.futila@gmail.com>
2023-02-13 14:57:08 -0300

    Capturando dados do formulário

--- src/pages/TimeLine.tsx
+++ src/pages/TimeLine.tsx
@@ -1,24 +1,38 @@
+import { FormEvent, useState } from "react";
 import { Header } from "../components/Header";
 import { Separator } from "../components/Separator";
 import { Tweet } from "../components/Tweet";
 
 import "./TimeLine.css";
 
-const tweets = [
-  "Meu primeiro tweet",
-  "Esse é o meu tweet",
-  "Funcionouuuuu o Deploy",
-];
-
 export function TimeLine() {
+  const [newTweet, setNewTweet] = useState("");
+
+  const [tweets, setTweets] = useState([
+    "Meu primeiro tweet",
+    "Esse é o meu tweet",
+    "Funcionouuuuu o Deploy",
+  ]);
+
+  function createNewTweet(event: FormEvent) {
+    event.preventDefault();
+
+    setTweets([newTweet, ...tweets]);
+    setNewTweet("");
+  }
   return (
     <main className="timeline">
       <Header title="Home" />
 
-      <form className="new-tweet-form">
+      <form onSubmit={createNewTweet} className="new-tweet-form">
         <label htmlFor="tweet">
           <img src="https://github.com/Futila.png" alt="Fernando Futila" />
-          <textarea id="tweet" placeholder="What's happening?"></textarea>
+          <textarea
+            onChange={(event) => setNewTweet(event.target.value)}
+            value={newTweet}
+            id="tweet"
+            placeholder="What's happening?"
+          ></textarea>
         </label>
 
         <button type="submit">Tweet</button>
