278d0db0a8c69d10463fc64edb0b09ceef2a62d1
Fernando Futila <fernando.futila@gmail.com>
2023-02-13 15:06:22 -0300

    Salvando novas respostas

--- src/pages/Status.tsx
+++ src/pages/Status.tsx
@@ -1,22 +1,41 @@
+import { FormEvent, useState } from "react";
 import { Header } from "../components/Header";
 import { Separator } from "../components/Separator";
 import { Tweet } from "../components/Tweet";
 
 import "./Status.css";
 
-const answers = ["Concordo", "Olha, faz sentido", "Parabéns pelo progresso"];
-
 export function Status() {
+  const [newAnswer, setNewAnswer] = useState("");
+
+  const [answers, setAnswers] = useState([
+    "Concordo",
+    "Olha, faz sentido",
+    "Parabéns pelo progresso",
+  ]);
+
+  function createNewAnswer(event: FormEvent) {
+    event.preventDefault();
+
+    setAnswers([newAnswer, ...answers]);
+    setNewAnswer("");
+  }
+
   return (
     <main className="status">
       <Header title="Tweet" />
       <Tweet content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem sed perspiciatis molestias ea fugit ab sapiente ipsam provident velit nemo. Quo similique aliquid voluptatem hic nisi. Molestiae neque quidem sed." />
 
       <Separator />
-      <form className="answer-tweet-form">
+      <form onSubmit={createNewAnswer} className="answer-tweet-form">
         <label htmlFor="tweet">
           <img src="https://github.com/Futila.png" alt="Fernando Futila" />
-          <textarea id="tweet" placeholder="Tweet your answer"></textarea>
+          <textarea
+            value={newAnswer}
+            onChange={(event) => setNewAnswer(event.target.value)}
+            id="tweet"
+            placeholder="Tweet your answer"
+          ></textarea>
         </label>
 
         <button type="submit">Answer</button>
